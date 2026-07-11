a43e389c270109b2130f71ccbfff50f42bb4913a
mackroph <mackroph@buildcv.local>
2026-07-11 13:42:47 -0500

    chore: add eslint config + lint scripts
    - Add flat-config eslint.config.js with @angular-eslint, typescript-eslint
      and @eslint/js presets (recommended + type-checked).
    - Add 'typecheck', 'lint:style' (prettier --check), 'lint:eslint'
      (eslint --max-warnings=0) and combined 'lint' npm scripts.
    - Add eslint devDependencies to package.json.

---
+++ eslint.config.js
@@ -0,0 +1,70 @@
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angularESLint = require('@angular-eslint/eslint-plugin');
+const angularTemplate = require('@angular-eslint/eslint-plugin-template');
+const tsParser = require('@typescript-eslint/parser');
+const angularTemplateParser = require('@angular-eslint/template-parser');
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+module.exports = tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/coverage/**', '**/.angular/**', '**/node_modules/**'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    plugins: {
+      '@angular-eslint': angularESLint,
+    },
+    rules: {
+      ...angularESLint.configs.recommended.rules,
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/no-empty-lifecycle-method': 'error',
+      '@angular-eslint/no-lifecycle-call': 'error',
+      '@angular-eslint/use-lifecycle-interface': 'error',
+    },
+    processor: angularTemplate.processors['extract-inline-html'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.spec.json'],
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+        ecmaVersion: 2022,
+      },
+    },
+  },
+  {
+    files: ['src/**/*.html'],
+    plugins: {
+      '@angular-eslint/template': angularTemplate,
+    },
+    rules: {
+      ...angularTemplate.configs.recommended.rules,
+      ...angularTemplate.configs.accessibility.rules,
+      '@angular-eslint/template/no-positive-tabindex': 'off',
+    },
+    languageOptions: {
+      parser: angularTemplateParser,
+    },
+  },
+);
